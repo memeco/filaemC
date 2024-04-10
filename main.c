58b0d6946c0ddc930b6d974eca21f9364273c58a
Lucas Quadros <olucasquadros@gmail.com>
2024-04-10 19:27:48 -0300

    função inicializar fila

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 // Define o tamanho máximo da fila como 5
 #define MAX_SIZE 5
 
@@ -11,3 +10,9 @@ typedef struct {
     int front; //Índice do elemento da frente da fila
     int rear; // Índice do elemento do final da fila
 } Queue;
+
+// Função para inicializar a fila
+void inicializarQueue(Queue *q) {
+    q->front = -1; //Inicializa o índice do elemento da frente como -1 (indicando fila vazia)
+    q->rear = -1; // Inicializa o índice do elemento do final como -1 (indicando fila vazia)
+}
